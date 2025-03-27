44ec7bd5455484b5dc5f7f099cf34697a29c7d6a
Dalifo <flor.mondaut@gmail.com>
2025-03-27 16:55:43 +0100

    :sparkles: Obstacles now appears only when the Rover finds them

--- src/rover/Map.ts
+++ src/rover/Map.ts
@@ -5,46 +5,63 @@ import { IMap } from "./IMap.js";
 export class Map implements IMap {
     private readonly width: number;
     private readonly height: number;
-    private readonly obstacles: Set<string>; 
-
+    private obstacles: Set<string>;
+    private discoveredObstacles: Set<string>;
+  
     constructor(width: number, height: number, obstacles: Position[] = []) {
-        this.width = width;
-        this.height = height;
-        this.obstacles = new Set(obstacles.map(({ x, y }) => `${x},${y}`)); 
+      this.width = width;
+      this.height = height;
+      this.obstacles = new Set(obstacles.map(({ x, y }) => `${x},${y}`));
+      this.discoveredObstacles = new Set();
     }
-    
+  
     getWidth(): number {
-        return this.width;
+      return this.width;
     }
-
+  
     getHeight(): number {
-        return this.height;
+      return this.height;
     }
-
+  
     private modulo(num: number, mod: number): number {
-        const valeurRéduiteSignée = (num % mod) % -mod;
-        const valeurNonSignée = valeurRéduiteSignée + mod;
-        return valeurNonSignée % mod;
+      const valeurRéduiteSignée = (num % mod) % -mod;
+      const valeurNonSignée = valeurRéduiteSignée + mod;
+      return valeurNonSignée % mod;
     }
-
+  
+    discoverObstacle(x: number, y: number): void {
+      this.discoveredObstacles.add(`${x},${y}`);
+    }
+  
     public isObstacle(x: number, y: number): boolean {
-        return this.obstacles.has(`${x},${y}`);
+      return this.obstacles.has(`${x},${y}`);
     }
-
+  
+    public hasDiscoveredObstacle(x: number, y: number): boolean {
+      return this.discoveredObstacles.has(`${x},${y}`);
+    }
+  
     private wrapPosition(x: number, y: number): Position {
-        return {
-            x: this.modulo(x, this.width),
-            y: this.modulo(y, this.height)
-        };
+      return {
+        x: this.modulo(x, this.width),
+        y: this.modulo(y, this.height),
+      };
     }
-
-    public getNextValidPosition(x: number, y: number, newX: number, newY: number): { x: number; y: number; obstacle: boolean } {
-        const wrappedPosition = this.wrapPosition(newX, newY);
-        
-        if (this.isObstacle(wrappedPosition.x, wrappedPosition.y)) {
-            return { x, y, obstacle: true };
-        }
-        
-        return { x: wrappedPosition.x, y: wrappedPosition.y, obstacle: false };
+  
+    public getNextValidPosition(
+      x: number,
+      y: number,
+      newX: number,
+      newY: number
+    ): { x: number; y: number; obstacle: boolean } {
+      const wrappedPosition = this.wrapPosition(newX, newY);
+  
+      const isObstacle = this.isObstacle(wrappedPosition.x, wrappedPosition.y) || this.hasDiscoveredObstacle(wrappedPosition.x, wrappedPosition.y);
+      if (isObstacle) {
+        return { x, y, obstacle: true };
+      }
+  
+      return { x: wrappedPosition.x, y: wrappedPosition.y, obstacle: false };
     }
-}
+  }
+  
--- src/utils/utlis.ts
+++ src/utils/utlis.ts
@@ -1,6 +1,7 @@
 import { WebSocket } from "ws";
 import { Rover } from "../rover/Rover.js";
 import { Map, Position } from "../rover/Map.js";
+import { Orientation } from "../rover/Orientation.js";
 
 export function generateRandomObstacles(width: number, height: number, count: number): { x: number, y: number }[] {
     const obstacles: { x: number, y: number }[] = [];
@@ -16,61 +17,65 @@ export function generateRandomObstacles(width: number, height: number, count: nu
 }
 
 export function generateMapString(rover: Rover, world: Map): string {
-    const position = rover.getPosition();
-    const direction = rover.getOrientation();
-    let grid = "";
-  
-    const directionSymbols: Record<string, string> = {
-      "N": " ↑ ",
-      "E": " → ",
-      "S": " ↓ ",
-      "W": " ← "
-    };
-  
-    for (let y = world.getHeight() - 1; y >= 0; y--) {
-      for (let x = 0; x < world.getWidth(); x++) {
-        if (x === position.x && y === position.y) {
-          grid += directionSymbols[direction];
-        } else if (world.isObstacle(x, y)) {
-          grid += " X ";
-        } else {
-          grid += " . ";
-        }
+  const position = rover.getPosition();
+  const direction = rover.getOrientation();
+  let grid = "";
+
+  const directionSymbols: Record<string, string> = {
+    "N": " ↑ ",
+    "E": " → ",
+    "S": " ↓ ",
+    "W": " ← "
+  };
+
+  for (let y = world.getHeight() - 1; y >= 0; y--) {
+    for (let x = 0; x < world.getWidth(); x++) {
+      if (x === position.x && y === position.y) {
+        grid += directionSymbols[direction];
+      } else if (world.hasDiscoveredObstacle(x, y)) {
+        grid += " X ";
+      } else {
+        grid += " . ";
       }
-      grid += "\n";
     }
-    return grid;
+    grid += "\n";
+  }
+
+  return grid;
 }
+
 
 export function checkForObstacle(rover: Rover, world: Map, command: string): Position | null {
-    const position: Position = rover.getPosition();
-    const direction: string = rover.getOrientation();
-    let newPosition: Position = { ...position };
+  const position: Position = rover.getPosition();
+  const direction: Orientation = rover.getOrientation();
+  let newPosition: Position = { ...position };
 
-    switch (command) {
-        case "F":
-            if (direction === "N") newPosition.y += 1;
-            else if (direction === "S") newPosition.y -= 1;
-            else if (direction === "E") newPosition.x += 1;
-            else if (direction === "W") newPosition.x -= 1;
-            break;
-        case "B":
-            if (direction === "N") newPosition.y -= 1;
-            else if (direction === "S") newPosition.y += 1;
-            else if (direction === "E") newPosition.x -= 1;
-            else if (direction === "W") newPosition.x += 1;
-            break;
-        case "L":
-        case "R":
-            return null;
-    }
+  switch (command) {
+    case "F":
+      if (direction === Orientation.North) newPosition.y += 1;
+      else if (direction === Orientation.South) newPosition.y -= 1;
+      else if (direction === Orientation.East) newPosition.x += 1;
+      else if (direction === Orientation.West) newPosition.x -= 1;
+      break;
+    case "B":
+      if (direction === Orientation.North) newPosition.y -= 1;
+      else if (direction === Orientation.South) newPosition.y += 1;
+      else if (direction === Orientation.East) newPosition.x -= 1;
+      else if (direction === Orientation.West) newPosition.x += 1;
+      break;
+    case "L":
+    case "R":
+      return null;
+  }
 
-    if (world.isObstacle(newPosition.x, newPosition.y)) {
-        return newPosition; 
-    }
-    
-    return null; 
+  if (world.isObstacle(newPosition.x, newPosition.y) || world.hasDiscoveredObstacle(newPosition.x, newPosition.y)) {
+    world.discoverObstacle(newPosition.x, newPosition.y);
+    return newPosition;
+  }
+
+  return null;
 }
+
 
 export function sendJsonResponse(ws: WebSocket, message: object) {
     ws.send(JSON.stringify(message));
